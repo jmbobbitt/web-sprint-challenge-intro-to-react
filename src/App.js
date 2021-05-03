c8351d68ee2f24279ef29668c98ffdb75647e68f
Jenni B <71854518+jmbobbitt@users.noreply.github.com>
2021-05-02 20:47:08 -0400

    completed app js & css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect , useState } from 'react';
 import './App.css';
+import Character from './components/Character';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,27 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [data , setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then((res) => {
+      console.log(res)
+      setData(res.data)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }, [])
+
+  console.log(data)
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {data.map((char =[]) => { 
+        return <Character key={char.id} data={char}/> }
+      )}
     </div>
   );
 }
